ed79b624c86bcfae48570023139910933c5efea9
Dean Andreakis <deanandreakis@gmail.com>
2011-12-29 21:01:34 -0600

    Initial Commit

---
+++ MovieMateTests/MovieMateTests.h
@@ -0,0 +1,13 @@
+//
+//  MovieMateTests.h
+//  MovieMateTests
+//
+//  Created by Dean Andreakis on 12/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MovieMateTests : SenTestCase
+
+@end
---
+++ MovieMateTests/MovieMateTests.m
@@ -0,0 +1,32 @@
+//
+//  MovieMateTests.m
+//  MovieMateTests
+//
+//  Created by Dean Andreakis on 12/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MovieMateTests.h"
+
+@implementation MovieMateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MovieMateTests");
+}
+
+@end
